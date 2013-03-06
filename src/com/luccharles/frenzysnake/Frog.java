619f1cb71b1dd02977de282178ea57334f3aacc1
Luc Tremblay <luc_t52@hotmail.com>
2013-03-06 16:50:51 -0500

    all

---
+++ src/com/luccharles/frenzysnake/Frog.java
@@ -0,0 +1,45 @@
+package com.luccharles.frenzysnake;
+
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+/**
+ * (c) 2010 Nicolas Gramlich 
+ * (c) 2011 Zynga Inc.
+ * 
+ * @author Nicolas Gramlich
+ * @since 14:08:58 - 11.07.2010
+ */
+public class Frog extends CellEntity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public Frog(final int pCellX, final int pCellY, final ITextureRegion pTiledTextureRegion, final VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pCellX, pCellY, CELL_WIDTH, CELL_HEIGHT, pTiledTextureRegion, pVertexBufferObjectManager);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
---
+++ src/com/luccharles/frenzysnake/MenuFrenzySnake.java
@@ -0,0 +1,65 @@
+package com.luccharles.frenzysnake;
+
+import org.andengine.AndEngine;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import org.andengine.AndEngine;
+import org.andengine.util.debug.Debug;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ExpandableListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+
+public class MenuFrenzySnake extends Activity {
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.activity_menu_frenzy_snake);
+
+		
+	}
+		
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_frenzy_snake, menu);
+		return true;
+	}
+	
+	public void NewGame(View view)
+	{
+		 Intent i = new Intent(this, SnakeGameActivity.class);
+		 startActivity(i);
+		// finish();
+	
+	}
+	
+
+	
+
+}
